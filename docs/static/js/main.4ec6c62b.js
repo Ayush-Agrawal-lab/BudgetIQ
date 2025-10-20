c7d83ee7fbb093e24137e54e886f8096af83b0a3
Abhishek Roy <161482475+icaruscantcode@users.noreply.github.com>
2025-10-20 18:36:40 +0000

    chore: test deps + root pytest config; replace docs placeholders with real build assets

--- docs/static/js/main.4ec6c62b.js
+++ docs/static/js/main.4ec6c62b.js
@@ -1,3 +1,1 @@
-// Placeholder loader for main JS; actual minified file is referenced from CDN in asset-manifest.json
-// When served from GitHub Pages, the index.html references the remote JS file already.
-console.log('BudgetIQ placeholder main.js loaded');
+/*! For license information please see main.4ec6c62b.js.LICENSE.txt */
