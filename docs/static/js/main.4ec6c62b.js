18a6d0e403c187d92f5215e6645e7cec96b61969
Abhishek Roy <161482475+icaruscantcode@users.noreply.github.com>
2025-10-20 18:26:31 +0000

    chore: add docs/ built site for GitHub Pages deployment

---
+++ docs/static/js/main.4ec6c62b.js
@@ -0,0 +1,3 @@
+// Placeholder loader for main JS; actual minified file is referenced from CDN in asset-manifest.json
+// When served from GitHub Pages, the index.html references the remote JS file already.
+console.log('BudgetIQ placeholder main.js loaded');
